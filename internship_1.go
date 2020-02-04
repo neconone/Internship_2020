1c4bff33535bce8ad2ee9d57958eab3cfda4655e
neconone <p.chaiolan30@gmail.com>
2020-02-05 00:04:25 +0700

    1_ take input

--- internship_1.go
+++ internship_1.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func findAbbre(fullname []string) {
+	fmt.Println(fullname[4])
+}
+
 func main() {
-	fmt.Println("Hello, Go")
+	var fullname []string
+	var size int
+	fmt.Print("Enter the number of your input : ")
+	fmt.Scanln(&size)
+	fullname = make([]string, size)
+	for i := 0; i < size; i++ {
+		fmt.Printf("Input number %d : ", (i + 1))
+		fmt.Scanln(&fullname[i])
+	}
+	findAbbre(fullname)
 }
